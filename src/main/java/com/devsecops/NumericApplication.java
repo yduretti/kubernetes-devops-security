8e77d46f761697be73ee3a078926588c8b042c6b
yduretti <yuriduretti@gmail.com>
2025-02-10 11:22:48 -0300

    change version from

--- src/main/java/com/devsecops/NumericApplication.java
+++ src/main/java/com/devsecops/NumericApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NumericApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(NumericApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(NumericApplication.class, args);
+    }
 }
